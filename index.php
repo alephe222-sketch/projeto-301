d9b976675820c9f2769c9e75112835328ff276a3
alephe <alephe222@gmail.com>
2025-10-29 10:30:17 -0300

    criação de estilos da pagina inicial

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php include ('db_connection.php'); 
+
+use LDAP\Result;?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,10 +11,34 @@
     
 </head>
 <body>
-    <h1> Minha To_Do_list </h1>
+    <h1> Minha To Do list </h1>
     <form action="add_task.php" method="POST">
     <input type="text" name="title" placeholder="Nova tarefa..." required> 
     <button type="submit"> Adicionar</button>  
-    </form>    
+    </form>
+
+    <hr>
+
+    <ul>
+        <?php
+        $sql = "SELECT * FROM task ORDER BY id DESC";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+                echo "<li>";
+                echo $row['status'] == 'concluida' ? "<s>{$row['title']}</s>" : $row['title'];
+            echo "
+            <a href='update_task.php?id={row['id']}'>X</a>
+            <a href='delete_task.php?id={row['id']}'>Y</a>";
+            echo "</li>";
+            }
+           }   else { 
+                echo " <p>Nenhuma tarefa cadastrada.</p>";
+}
+        
+            
+        ?>
+    </ul>
 </body>
 </html>
